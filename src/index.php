7e4990b6ebb922a2b22c419f23d2eacfc4dc4b79
thearsenalfanz <sodanamkang@email.com>
2017-02-02 10:47:59 +0700

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('./LINEBot.php');
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('P3avdIxyxnYbA4xJmyGCWyD2zg5Yi785QZIKdXNajsJV/t8zQKRqfLus2MmwarjTq8jUlLtx1p/YhF+R7tRCv0aQOia3KQhIkPR2PL45xst9NCURrzMXPoVqI0oVFZ1To6tHwSwCeJg0QuXo7HyYuAdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1f96bb225b40366566a84317e187ae1c']);
 
