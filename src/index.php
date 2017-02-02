f4bf7ca8e26245be283dd088e6dcec42217f7c0e
thearsenalfanz <sodanamkang@email.com>
2017-02-02 11:00:04 +0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once('./LINEBot.php');
+// require_once('./LINEBot.php');
 
-$httpClient = new CurlHTTPClient('P3avdIxyxnYbA4xJmyGCWyD2zg5Yi785QZIKdXNajsJV/t8zQKRqfLus2MmwarjTq8jUlLtx1p/YhF+R7tRCv0aQOia3KQhIkPR2PL45xst9NCURrzMXPoVqI0oVFZ1To6tHwSwCeJg0QuXo7HyYuAdB04t89/1O/w1cDnyilFU=');
-$bot = new LINEBot($httpClient, ['channelSecret' => '1f96bb225b40366566a84317e187ae1c']);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('P3avdIxyxnYbA4xJmyGCWyD2zg5Yi785QZIKdXNajsJV/t8zQKRqfLus2MmwarjTq8jUlLtx1p/YhF+R7tRCv0aQOia3KQhIkPR2PL45xst9NCURrzMXPoVqI0oVFZ1To6tHwSwCeJg0QuXo7HyYuAdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1f96bb225b40366566a84317e187ae1c']);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$event = $bot->parseEventRequest($content, $_SERVER['HTTP_X_LINE_SIGNATURE']);
 
 
-$textMessageBuilder = new TextMessageBuilder('hello');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 if ($response->isSucceeded()) {
     echo 'Succeeded!';
