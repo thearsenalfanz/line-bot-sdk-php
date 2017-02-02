6a6a1749dc31a6ab8ca4d300f10c9314b3970c34
thearsenalfanz <sodanamkang@email.com>
2017-02-02 11:15:39 +0700

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,25 +1,11 @@
 <?php
-
-namespace LINE;
-use LINE\LINEBot\Event\Parser\EventRequestParser;
-use LINE\LINEBot\HTTPClient;
-use LINE\LINEBot\MessageBuilder;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use LINE\LINEBot\Response;
-use LINE\LINEBot\SignatureValidator;
-
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('P3avdIxyxnYbA4xJmyGCWyD2zg5Yi785QZIKdXNajsJV/t8zQKRqfLus2MmwarjTq8jUlLtx1p/YhF+R7tRCv0aQOia3KQhIkPR2PL45xst9NCURrzMXPoVqI0oVFZ1To6tHwSwCeJg0QuXo7HyYuAdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1f96bb225b40366566a84317e187ae1c']);
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
 	$content = file_get_contents('php://input');
-
-	$event = $bot->parseEventRequest($content, $_SERVER['HTTP_X_LINE_SIGNATURE']);
-
-
+	$bot->parseEventRequest($content, $_SERVER['HTTP_X_LINE_SIGNATURE']));
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+$response = $bot->replyMessage('<reply token>', $textMessageBuilder);
 if ($response->isSucceeded()) {
     echo 'Succeeded!';
     return;
@@ -28,9 +14,5 @@ else
 {
 	echo $response->getHTTPStatus . ' ' . $response->getRawBody();
 }
-
 }
-
-
-
 ?>
