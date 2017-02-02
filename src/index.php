4ca89cd96c8ac30b14a203ec71de017a3100448e
Por Bergkamp <sodanamkang@email.com>
2017-02-02 22:35:25 +0700

    comment out writetext

--- src/index.php
+++ src/index.php
@@ -8,33 +8,33 @@ spl_autoload_register(function ($class_name) {
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('P3avdIxyxnYbA4xJmyGCWyD2zg5Yi785QZIKdXNajsJV/t8zQKRqfLus2MmwarjTq8jUlLtx1p/YhF+R7tRCv0aQOia3KQhIkPR2PL45xst9NCURrzMXPoVqI0oVFZ1To6tHwSwCeJg0QuXo7HyYuAdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1f96bb225b40366566a84317e187ae1c']);
 
-$name = date("Ymd_His");
+// $name = date("Ymd_His");
 
-$myfile = fopen($name.".txt", "w") or var_dump(error_get_last());
-$txt = $_SERVER['REQUEST_METHOD']."\n";
+// $myfile = fopen($name.".txt", "w") or var_dump(error_get_last());
+// $txt = $_SERVER['REQUEST_METHOD']."\n";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$content = file_get_contents('php://input');
-	$txt .= $content."\n\n";
+	// $txt .= $content."\n\n";
 	$event = $bot->parseEventRequest($content, $_SERVER['HTTP_X_LINE_SIGNATURE']);
-	$txt .= var_dump($event)."\n\n";
+	// $txt .= var_dump($event)."\n\n";
 // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 
 
 $response = $bot->replyMessage($event['replyToken'], "hello!");
 if ($response->isSucceeded()) {
     echo 'Succeeded!';
-    $txt .= 'Succeeded!'."\n\n";
+    // $txt .= 'Succeeded!'."\n\n";
     return;
 }
 else
 {
-	$txt .= var_dump($event);
+	// $txt .= var_dump($event);
 	echo $response->getHTTPStatus . ' ' . $response->getRawBody();
-	$txt .= $response->getHTTPStatus . ' ' . $response->getRawBody()."\n\n";
+	// $txt .= $response->getHTTPStatus . ' ' . $response->getRawBody()."\n\n";
 }
 
-fwrite($myfile, $txt);
-fclose($myfile);
+// fwrite($myfile, $txt);
+// fclose($myfile);
 }
 ?>
